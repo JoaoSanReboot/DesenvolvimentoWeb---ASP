937528f24882fdd5a14e738a62f19a0314e64b1a
João Santana <joaopedrosantanarosa@gmail.com>
2025-02-13 21:24:45 -0300

    Att

--- ServidorHttpSimples/obj/Debug/net9.0/ServidorHttpSimples.AssemblyInfo.cs
+++ ServidorHttpSimples/obj/Debug/net9.0/ServidorHttpSimples.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServidorHttpSimples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa89ec66b52b03c72c74744788a0d37d23c0475e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f34d3171c025cbd83dca5d8fe82644353fb04f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServidorHttpSimples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServidorHttpSimples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
